87249ae250a551f51bf6cc95c57cd2ad8e33166f
Dheeraj kumar <58466505+DheerajKumar066@users.noreply.github.com>
2019-12-06 07:00:36 -0800

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
-
-int main() 
-{
-    int n, revN = 0, remind, origN;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    originalN = n;
-    // reversed integer is stored in reversedN
-    while (n != 0) {
-        remind = n % 10;
-        revN = revN * 10 + remind;
-        n /= 10;
-    }
-    // palindrome if orignalN and reversedN are equal
-    if (origN == revN)
-        printf("%d is a palindrome.", origN);
-    else
-        printf("%d is not a palindrome.", origN);
-    return 0;
-}
+    #include<stdio.h>  
+    int main()    
+    {    
+        int n,r,sum=0,temp;    
+        printf("enter the number=");    
+        scanf("%d",&n);    
+        temp=n;    
+        while(n>0)    
+    {    
+        r=n%10;    
+        sum=(sum*10)+r;    
+        n=n/10;    
+    }    
+        if(temp==sum)    
+        printf("palindrome number ");    
+        else    
+        printf("not palindrome");   
+        return 0;  
+    }   
